483d15dcc6d774784aebd471bae279083144b32c
eXsik <qba.skupinski@gmail.com>
2024-07-25 12:26:36 +0200

    added my project

---
+++ src/features/user/Username.jsx
@@ -0,0 +1,12 @@
+import { useSelector } from "react-redux";
+import { getUser } from "./userSlice";
+
+export default function Username() {
+  const { username } = useSelector(getUser);
+
+  if (!username) return null;
+
+  return (
+    <div className="hidden text-sm font-semibold md:block">{username}</div>
+  );
+}
